31583dd06ca9aab14d976b84766159a503f12fe5
lipenghui <18401446221@163.com>
2020-06-28 22:39:19 +0800

    函组件

--- src/index.js
+++ src/index.js
@@ -1,42 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 /**
- * jsx描述界面上的元素长什么样子
- * jsx javascript+xml 是一种把js和html混合书写的一种语法
- * jsx可增加属性 js dom xx.className
- * jsx里面如果想使用一个变量，我们需要把它放在大括号里面
- * 表达式就是变量和运算符的组合
+ * 我们把页面分成若干的独立部分，单独编写，单独维护
+ * 函数组件
+ * 一个返回普通react元素的函数就是一个合法的react组件
+ * 组件需要返回一个并且仅能返回一个react根元素
+ * 组件的名称需要大写
+ * 
+ * 收集属性对象props {name:'zhufeng',age:10}
+ * 会把props对象传入welcome函数，并得到一个返回值 react元素
 */
-// let styles ={color:'red'}
 
-// ReactDOM.render(
-//   <div style={styles}>
-//     aaa
-//   </div>,
-//   document.getElementById('root')
-// );
+function Welcome(props){
+return <h1>helle {props.name}</h1>
+}
 
-let ele = React.createElement('h1',{
-  className:'title'
-},'hello',React.createElement('span',null,'world'))
-console.log(JSON.stringify(ele),'ele')
-// react 元素 就是一个普通的js 对象
-// {
-//   "type":"h1",
-//   "props":{
-//    "className":"title",
-//    "children":[
-//      "hello",{
-//          "type":"span",
-//       "props":{"children":"world"}
-//   }
-
-// 元素的更新
-// 元素是不可变的
-setInterval(()=>{
-  let clock = <div>{new Date().toLocaleTimeString()}</div>
-  ReactDOM.render(clock,document.getElementById('root'))
-},1000)
-
-// ReactDOM.render(ele,document.getElementById('root'))
+ReactDOM.render(<Welcome name="zhufeng"/>, document.getElementById('root'))
 
